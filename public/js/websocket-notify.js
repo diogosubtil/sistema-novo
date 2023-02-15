061d9bd1dfe126e0355e3ea3a7ed68cd39deaf3d
diogosubtil <diogosubtil20@gmail.com>
2023-02-15 09:54:38 -0300

    Ajuste websocket

--- public/js/websocket-notify.js
+++ public/js/websocket-notify.js
@@ -1,8 +1,8 @@
 //WebSocket
-var connectionWeb = new WebSocket('ws://127.0.0.1:8090');
+var connectionWeb = new WebSocket('ws://185.213.81.95:9090');
 
 connectionWeb.onopen = function(e) {
-    // console.log("Connection established!");
+    console.log("Connection established!");
 };
 
 connectionWeb.onmessage = function(e) {
